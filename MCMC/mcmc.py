bf80c2119400569397d9a5aa353d6a0603e00b90
iamsikun <xusikun96@gmail.com>
2020-08-11 10:18:38 -0400

    Create MCMC and Variational Inference package

---
+++ MCMC/mcmc.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/10/20 17:45
+# @Author  : Sikun Xu
+# @FileName: mcmc.py
+# @Software: PyCharm
+
+"""
+[Murphy K. P. 2012, Ch24] The basic idea behind MCMC is to construct a Markov Chain on the state space X whose
+stationary distribution is the target density p^{*}(x) of interest
+"""
+
+
+class GibbsSampling:
+    """
+    Intro
+    ----------
+    Also known as Glauber dynamics or the heat bath method.
+    [Murphy K. P. 2012, Ch24.2] Gibbs sampling is the MCMC analog of coordinate descent
+    """
+    def __init__(self):
+        """
+
+        """
+        pass
---
+++ VariationalInference/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/10/20 17:57
+# @Author  : Sikun Xu
+# @FileName: __init__.py.py
+# @Software: PyCharm
