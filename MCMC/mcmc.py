5d7b31b9c2f2d61abe6f78268246d31a311f905d
iamsikun <xusikun96@gmail.com>
2020-08-22 12:07:16 -0400

    Add Gaussian Proposal and modify MH accordingly

--- MCMC/mcmc.py
+++ MCMC/mcmc.py
@@ -4,7 +4,11 @@
 # @FileName: mcmc.py
 # @Software: PyCharm
 
-from .mcmc_helper import *
+from MCMC.mcmc_helper import *
+from scipy import integrate
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class GibbsSampling:
@@ -25,16 +29,15 @@ class MetropolisHastings(SamplerSuperClass):
     Andrieu, Christophe, et al. "An introduction to MCMC for machine learning." Machine learning 50.1-2 (2003): 5-43.
     """
 
-    def __init__(self, target_dstn_func, proposal_dstn_func='gaussian', algorithm='metropolis', gaussian_proposal_sd=10,
-                 gaussian_proposal_mean=0):
+    def __init__(self, target_dstn, proposal_dstn, algorithm='metropolis'):
         """
         TODO
 
         Parameters
         ----------
-        target_dstn_func:
+        target_dstn: PDF of the target distribution, no need to be normalized
 
-        proposal_dstn_func: default to gaussian
+        proposal_dstn: has sample and pdf method
 
         algorithm: independent or metropolis, default to metropolis. This parameter will only be used when
         proposal_dstn_func parameter is not specified
@@ -43,36 +46,23 @@ class MetropolisHastings(SamplerSuperClass):
             2. Metropolis refers to Metropolis algorithm, meaning the proposal distribution is a symmetric random walk,
             i.e. q(next|current) = q(current|next)
 
-        gaussian_proposal_sd: standard deviation of the proposal gaussian distribution, default to 10. This parameter
-        will only be used when the proposal distribution is not specified.
-
-        gaussian_proposal_mean: mean of the proposal gaussian distribution, default to 0. This parameter is only used
-        when algorithm==independent
-
         """
         # parameters
         self.algorithm = algorithm  # self.algorithm must be defined before self.proposal_dstn_func
-        self.gaussian_proposal_mean = gaussian_proposal_mean
-        self.gaussian_proposal_cov = gaussian_proposal_sd
         # target and proposal functions
-        self.target_dstn_func = target_dstn_func
-        self.proposal_dstn_func = proposal_dstn_func
+        self.target_dstn = target_dstn
+        self.proposal_dstn = proposal_dstn
 
         # init super
         super(MetropolisHastings, self).__init__()
 
         # attributes
-        self.dimension = None
+        self.dimension = self.proposal_dstn.dimension
 
-    def fit(self, dim, num_samples):
+    def fit(self, num_samples):
         """
         TODO
         """
-        # update attributes
-        self.dimension = dim
-        self.proposal_dstn_func = self._init_proposal_dstn_with_gaussian() if self.proposal_dstn_func == 'gaussian' \
-            else self.proposal_dstn_func
-
         # 1. initialization
         x_arr = np.zeros(shape=(num_samples, self.dimension))
 
@@ -80,7 +70,7 @@ class MetropolisHastings(SamplerSuperClass):
         for idx in range(1, num_samples):
             current_x = x_arr[idx-1]
             # 2.1 sample next_state from proposal distribution q(next|current)
-            next_x = self._sample_from_proposal_dstn(current_x)  # x_arr[idx] take the idx-row, shape=(d, )
+            next_x = self.proposal_dstn.sample(mean=current_x)
 
             # 2.2 acceptance check
             x_arr[idx] = next_x if self._acceptance_check(current_x, next_x) else current_x
@@ -88,44 +78,14 @@ class MetropolisHastings(SamplerSuperClass):
         # return
         return x_arr
 
-    def _sample_from_proposal_dstn(self, current_x, size=1) -> float:
-        """
-        Sample from proposal distribution
-
-        Parameters
-        ----------
-        current_x: current state, numpy array with shape=(d, )
-        """
-        return self.proposal_dstn_func(current_x, size)
-
     def _calculate_proposal_dstn_probability(self, current_x, next_x):
         """
         TODO
         """
-        if self.algorithm == 'independent' and self.dimension == 1:
-            return norm.pdf(
-                next_x,
-                self.gaussian_proposal_mean,
-                self.gaussian_proposal_cov,
-            )
-        elif self.algorithm == 'independent' and self.dimension > 1:
-            return multivariate_normal.pdf(
-                next_x,
-                self.gaussian_proposal_mean,
-                self.gaussian_proposal_cov
-            )
-        elif self.algorithm == 'metropolis' and self.dimension == 1:
-            return norm.pdf(
-                next_x,
-                current_x,
-                self.gaussian_proposal_cov,
-            )
-        elif self.algorithm == 'metropolis' and self.dimension > 1:
-            return multivariate_normal.pdf(
-                next_x,
-                current_x,
-                self.gaussian_proposal_cov
-            )
+        if self.algorithm == 'independent':
+            return self.proposal_dstn.pdf(x=next_x,)
+        elif self.algorithm == 'metropolis':
+            return self.proposal_dstn.pdf(x=next_x, mean=current_x,)
         else:
             raise Exception('Error in initializing proposal distribution with algorithm {} and dimension {}'.format(
                 self.algorithm, self.dimension
@@ -142,75 +102,37 @@ class MetropolisHastings(SamplerSuperClass):
         TODO
         """
 
-        numerator = self.target_dstn_func(next_state) * self._calculate_proposal_dstn_probability(current_state,
-                                                                                                  next_state)
-        denominator = self.target_dstn_func(current_state) * self._calculate_proposal_dstn_probability(next_state,
-                                                                                                       current_state)
+        numerator = self.target_dstn(next_state) * self._calculate_proposal_dstn_probability(current_state, next_state)
+        denominator = self.target_dstn(current_state) * self._calculate_proposal_dstn_probability(next_state,
+                                                                                                  current_state)
         return min(1, numerator / denominator)
-
-    def _init_proposal_dstn_with_gaussian(self):
-        """
-        Return function that generates gaussian probability. This method is only called when the proposal_dstn_func
-        is set to default value (gaussian)
-
-        Return
-        ----------
-        gaussian_dstn_func: a function that generates probability following gaussian distribution
-        """
-        if self.algorithm == 'independent' and self.dimension == 1:
-            self.gaussian_proposal_cov = self.gaussian_proposal_cov
-            return lambda mean, size: np.random.normal(
-                self.gaussian_proposal_mean,
-                self.gaussian_proposal_cov,
-                size
-            )
-        elif self.algorithm == 'independent' and self.dimension > 1:
-            self.gaussian_proposal_mean = np.repeat(self.gaussian_proposal_mean, self.dimension)
-            self.gaussian_proposal_cov = np.diag(
-                np.repeat(self.gaussian_proposal_cov, self.dimension)
-            )
-            return lambda mean, size: np.random.multivariate_normal(
-                self.gaussian_proposal_mean,
-                self.gaussian_proposal_cov,
-                size
-            )
-        elif self.algorithm == 'metropolis' and self.dimension == 1:
-            self.gaussian_proposal_cov = self.gaussian_proposal_cov
-            return lambda mean, size: np.random.normal(
-                mean,
-                self.gaussian_proposal_cov,
-                size
-            )
-        elif self.algorithm == 'metropolis' and self.dimension > 1:
-            self.gaussian_proposal_cov = np.diag(
-                np.repeat(self.gaussian_proposal_cov, self.dimension)
-            )
-            return lambda mean, size: np.random.multivariate_normal(
-                mean,
-                self.gaussian_proposal_cov,
-                size
-            )
-        else:
-            raise Exception('Error in initializing proposal distribution with algorithm {} and dimension {}'.format(
-                self.algorithm, self.dimension
-            ))
 
 
 if __name__ == '__main__':
-    def target_dstn_func_(x):
+
+    # unnormalized target distribution
+    def target_dstn_func_(x: float) -> float:
         return 0.3 * np.exp(-0.2 * x ** 2) + 0.7 * np.exp(-0.2 * (x - 10) ** 2)
 
+    # proposal distribution
+    proposal_dstn_obj = GaussianProposal(mean=0, scale=10)
+
+    # MCMC object
     MH_obj = MetropolisHastings(
-        target_dstn_func=target_dstn_func_,
-        gaussian_proposal_sd=10,
+        target_dstn=target_dstn_func_,
+        proposal_dstn=proposal_dstn_obj,
     )
-    result = MH_obj.fit(dim=1, num_samples=5000)
+    result = MH_obj.fit(num_samples=5000)
 
     # visualization
+    # data preparation
+    # x and y for normalized target distribution
+    plot_x = np.linspace(-5, 16, 500)
+    norm_const = integrate.quad(target_dstn_func_, -np.inf, np.inf)[0]  # normalization constant
+    plot_y = target_dstn_func_(plot_x) / norm_const  # use numerical integration to get the normalized distribution
+    # plot
     sns.set()
     fig, ax = plt.subplots()
-    sns.distplot(result, ax=ax, label='MH Sampler', norm_hist=True, kde=False, bins=40)
-    plot_x = np.linspace(-5, 16, 500)
-    plot_y = target_dstn_func_(plot_x) / 4.5
+    sns.distplot(result, ax=ax, label='MH Sampler', norm_hist=True, kde=False, bins=50)
     sns.lineplot(plot_x, plot_y, ax=ax, label='Target Distribution')
     plt.show()
--- MCMC/mcmc_helper.py
+++ MCMC/mcmc_helper.py
@@ -7,13 +7,11 @@
 import numpy as np
 from scipy.stats import norm, multivariate_normal
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 
 class SamplerSuperClass(object):
     def __init__(self):
         """
+        TODO
 
         """
         # parameters
@@ -22,3 +20,83 @@ class SamplerSuperClass(object):
         # attributes
         pass
 
+
+class GaussianProposal(object):
+    """
+    TODO
+    """
+
+    def __init__(self, mean, scale):
+        """
+        TODO
+
+        Parameters:
+        ----------
+        mean:
+
+        cov:
+
+        dim: dimension of the random variable (vector), if no dimension is provided, then the will take the size of the
+        mean provided
+
+        """
+        # parameters
+        self.mean = mean
+        self.scale = scale
+
+        # attributes
+        self.dimension = 1 if isinstance(self.mean, int) or isinstance(self.mean, float) else len(self.mean)
+
+    def sample(self, mean=None, size=1):
+        """
+        TODO
+
+        Parameters:
+        ----------
+        mean:
+
+        size:
+
+        """
+        # attributes
+        mean = self.mean if mean is None else mean
+
+        if self.dimension == 1:
+            return np.random.normal(
+                loc=mean,
+                scale=self.scale,
+                size=size,
+            )
+        else:
+            return np.random.multivariate_normal(
+                mean=mean,
+                cov=self.scale,
+                size=size,
+            )
+
+    def pdf(self, x, mean=None) -> float:
+        """
+        TODO
+
+        Parameters:
+        ----------
+        x:
+
+        mean:
+        """
+        # attributes
+        mean = self.mean if mean is None else mean
+
+        # calculate pdf
+        if self.dimension == 1:
+            return norm.pdf(
+                x,
+                mean,
+                self.scale,
+            )
+        else:
+            return multivariate_normal.pdf(
+                x,
+                mean,
+                self.scale,
+            )
